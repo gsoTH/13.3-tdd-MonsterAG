991b778c827b62ac945e04ebdd7bf688549908d7
M.Thomas <m.thomas@gso.schule.koeln>
2025-02-17 12:03:53 +0100

    Weitere Testdateien

---
+++ MonsterAGTests/CannibalGhostTests.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using MonsterAG;
+
+namespace MonsterAGTests
+{
+    [TestClass]
+    public class CannibalGhostTests
+    {
+
+        [TestMethod]
+        public void CannibalGhost_wirdKorrektErzeugt()
+        {
+            // Arrange
+            string name = "hungrySpooky";
+
+            // Act
+            CannibalGhost cannibalGhost = new CannibalGhost(name);
+
+            // Assert
+            Assert.AreEqual(name, cannibalGhost.Name);
+            Assert.AreEqual(1, cannibalGhost.Size);
+        }
+
+        [TestMethod]
+        public void Eat_mitGeistSteigertSize()
+        {
+            // Arrange
+            CannibalGhost cannibalGhost = new CannibalGhost("hungrySpooky");
+            Ghost ghostToEat = new Ghost("Spooky");
+            ghostToEat.Size = 5;
+            SlimeGhost slimeGhostToEat = new SlimeGhost("Slimey");
+            slimeGhostToEat.Size = 2;
+            CannibalGhost cannibalGhostToEat = new CannibalGhost("tastyCannibal");
+            cannibalGhostToEat.Size = 1;
+
+            // Act
+            cannibalGhost.Eat(ghostToEat);
+            cannibalGhost.Eat(slimeGhostToEat);
+            cannibalGhost.Eat(cannibalGhostToEat);
+
+            // Assert
+            Assert.AreEqual(9, cannibalGhost.Size);
+            Assert.IsNull(ghostToEat);
+            Assert.IsNull(slimeGhostToEat);
+            Assert.IsNull(cannibalGhostToEat);
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void Eat_WithNullGhostThrowsException()
+        {
+            // Arrange
+            CannibalGhost cannibalGhost = new CannibalGhost("hungrySpooky");
+            Ghost ghostToEat = null;
+
+            // Act
+            cannibalGhost.Eat(ghostToEat);
+        }
+    }
+}
---
+++ MonsterAGTests/SlimeGhostTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using MonsterAG;
+
+namespace MonsterAGTests
+{
+    [TestClass]
+    public class SlimeGhostTests
+    {
+
+        [TestMethod]
+        public void SlimeGhost_wirdKorrektErstellt()
+        {
+            // Arrange
+            string name = "SlimeSpooky";
+
+            // Act
+            SlimeGhost slimeGhost = new SlimeGhost(name);
+
+            // Assert
+            Assert.AreEqual(name, slimeGhost.Name);
+            Assert.AreEqual(1, slimeGhost.Size);
+        }
+
+        [TestMethod]
+        public void Slime_erzeugtSlimeMessageMitNamen()
+        {
+            // Arrange
+            string name = "SlimeSpooky";
+            SlimeGhost slimeGhost = new SlimeGhost(name);
+
+            // Act
+            string slimeMessage = slimeGhost.Slime();
+
+            // Assert
+            Assert.AreEqual("SlimeSpooky hinterlässt eine Schleimspur.", slimeMessage);
+        }
+
+
+        [TestMethod]
+        public void Haunt_erzeugtSlimeUndBuhMessage()
+        {
+            // Arrange
+            SlimeGhost slimeGhost = new SlimeGhost("SlimeSpooky");
+
+            // Act
+            string ausgabe = slimeGhost.Haunt();
+
+            // Assert
+            Assert.AreEqual("SlimeSpooky hinterlässt eine Schleimspur.\r\nSlimeSpooky sagt: 'Buh'", ausgabe);
+        }
+    }
+}
+
